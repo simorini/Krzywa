836188d1de407fb39a5e3961d2bee9bf1439b1c8
simorini <58668418+simorini@users.noreply.github.com>
2019-12-08 21:34:27 +0100

    Add files via upload

---
+++ Dane.java
@@ -0,0 +1,10 @@
+package libapp;
+
+public class Dane {
+	
+	protected static double kat, predkosc, zasieg;
+	
+	protected static double calculate() {
+		return 0;
+	}
+}
---
+++ DrawPanel.java
@@ -0,0 +1,22 @@
+package libapp;
+
+import javax.swing.JPanel;
+
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.xy.XYDataset;
+
+public class DrawPanel {
+	
+	private static JFreeChart createChart( XYDataset dataset) {
+		return null;
+	}
+	
+	private static XYDataset createDataset() {
+		return null;
+	}
+	
+	public static JPanel createWoPanel() {
+		return null;
+	}
+	
+}
